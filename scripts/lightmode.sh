0b0abe2b0432b6b4f5fb61ede62af3786f5c8c47
Carter Andrew <carterinmotion@gmail.com>
2024-07-30 08:03:54 -0700

    Updated light mode script

--- scripts/lightmode.sh
+++ scripts/lightmode.sh
@@ -1,4 +1,4 @@
-  cat ~/themes/themes/night_owlish_light.toml > ~/.config/alacritty/alacritty.toml
+ cat ~/themes/themes/github_light_colorblind.toml  > ~/.config/alacritty/alacritty.toml
 
   qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string: var Desktops = desktops(); for (i=0;i<Desktops.length;i++) { d = Desktops[i]; d.wallpaperPlugin = "org.kde.color"; d.currentConfigGroup = Array("Wallpaper", "org.kde.color", "General"); d.writeConfig("Color", "#ffffff"); }'
 
